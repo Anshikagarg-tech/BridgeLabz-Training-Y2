523971a44e453ae48d67a668e9a786c6b5fc4788
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-15 22:45:49 +0530

    Anshika[Added]- Problem to find illegal argument

---
+++ Level1_strings/IllegalArgumentDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class IllegalArgumentDemo {
+
+    public static void generateException(String text, int start, int end) {
+        System.out.println(text.substring(start, end)); 
+    }
+
+    public static void handleException(String text, int start, int end) {
+        try {
+            System.out.println(text.substring(start, end));
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException handled: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException handled: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        handleException(text, start, end);
+    }
+}
