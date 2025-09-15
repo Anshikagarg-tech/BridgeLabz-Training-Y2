12f077464c2c14923b55550dfe835748de2ef420
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-15 21:56:03 +0530

    Anshika[Added] - All problem statements of arrays in java

---
+++ Arrays/Level1_Arrays/StoreNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class StoreNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] numbers = new double[10];
+        double total = 0.0;
+        int index = 0;
+
+        System.out.println("Enter numbers (max 10). Enter 0 or negative to stop:");
+
+        while (true) {
+            double num = sc.nextDouble();
+            if (num <= 0 || index == 10) {
+                break;
+            }
+            numbers[index++] = num;
+        }
+
+        for (int i = 0; i < index; i++) {
+            total += numbers[i];
+        }
+
+        System.out.println("Numbers entered:");
+        for (int i = 0; i < index; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println("\nSum = " + total);
+
+        sc.close();
+    }
+}
