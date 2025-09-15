715ec92e06439e635fc8781247c8b332401dd6f6
Anshikagarg-tech <anshikagarg200521@gmail.com>
2025-09-15 22:57:10 +0530

    Anshika[Added]- Problem to display character type

---
+++ Level2_strings/CharTypeDisplay.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class CharTypeDisplay {
+
+    public static String checkCharType(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32);
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            if ("aeiou".indexOf(ch) != -1) {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        }
+        return "Not a Letter";
+    }
+
+    public static String[][] getCharTypes(String text) {
+        int length = 0;
+        try {
+            while (true) {
+                text.charAt(length);
+                length++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {}
+
+        String[][] result = new String[length][2];
+        for (int i = 0; i < length; i++) {
+            char ch = text.charAt(i);
+            result[i][0] = String.valueOf(ch);
+            result[i][1] = checkCharType(ch);
+        }
+        return result;
+    }
+
+    public static void displayCharTypes(String[][] arr) {
+        System.out.println("Character\tType");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + "\t\t" + arr[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        String[][] result = getCharTypes(text);
+
+        displayCharTypes(result);
+    }
+}
